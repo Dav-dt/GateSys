f36dc635f707ca84f0544ea2bfcdb8ab96eb349e
David <david.tuaillon@unistra.fr>
2026-05-05 16:55:25 +0200

    ajout bdd et classes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace saeStargateTUAILLON_LONGO_YURTSEBEN
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace saeStargateTUAILLON_LONGO_YURTSEBEN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
