b0e966d9633f47921cd780321227f922f89416b1
Fabio Longo <fabiolongofabio@gmail.com>
2026-05-07 15:27:14 +0200

    debut alien

---
+++ control/Alien.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace saeStargateTUAILLON_LONGO_YURTSEBEN.control
+{
+    partial class Alien
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbAlien = new System.Windows.Forms.PictureBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblRace = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAlien
+            // 
+            this.pbAlien.Location = new System.Drawing.Point(18, 16);
+            this.pbAlien.Name = "pbAlien";
+            this.pbAlien.Size = new System.Drawing.Size(144, 148);
+            this.pbAlien.TabIndex = 0;
+            this.pbAlien.TabStop = false;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(181, 18);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(35, 13);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "label1";
+            // 
+            // lblRace
+            // 
+            this.lblRace.AutoSize = true;
+            this.lblRace.Location = new System.Drawing.Point(181, 62);
+            this.lblRace.Name = "lblRace";
+            this.lblRace.Size = new System.Drawing.Size(35, 13);
+            this.lblRace.TabIndex = 2;
+            this.lblRace.Text = "label2";
+            // 
+            // Alien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblRace);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.pbAlien);
+            this.Name = "Alien";
+            this.Size = new System.Drawing.Size(366, 187);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAlien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAlien;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblRace;
+    }
+}
---
+++ control/Alien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saeStargateTUAILLON_LONGO_YURTSEBEN.control
+{
+    public partial class Alien : UserControl
+    {
+        public Alien()
+        {
+            InitializeComponent();
+        }
+
+        public Alien(string nom,string race, List<string>)
+        {
+
+        }
+    }
+}
