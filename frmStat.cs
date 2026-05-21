f61e36e228703a15a83269d33448ddfb459b52df
David <david.tuaillon68@gmail.com>
2026-05-21 23:08:16 +0200

    douteux

---
+++ frmStat.cs
@@ -0,0 +1,30 @@
+﻿using saeStargateTUAILLON_LONGO_YURTSEBEN.control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saeStargateTUAILLON_LONGO_YURTSEBEN
+{
+    public partial class frmStat : Form
+    {
+        public frmStat()
+        {
+            InitializeComponent();
+        }
+
+        private void frmStat_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new Statistique().getMissionPlanete();
+            NbMissionPlanete nbMission = new NbMissionPlanete(dt);
+            nbMission.Top = 10;
+            nbMission.Left = 10;
+            this.Controls.Add(nbMission);
+        }
+    }
+}
