a59637e172f2d1a0b963a63fb82db48ff697bd5d
nexxo <fabiolongofabio@gmail.com>
2026-05-21 16:48:35 +0200

    fix taille des user controle

--- control/ButtonBack.Designer.cs
+++ control/ButtonBack.Designer.cs
@@ -44,8 +44,7 @@
             // 
             // ButtonBack
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.btnBack);
             this.Name = "ButtonBack";
             this.Size = new System.Drawing.Size(76, 68);
--- control/Planete.Designer.cs
+++ control/Planete.Designer.cs
@@ -111,8 +111,7 @@
             // 
             // Planete
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Controls.Add(this.btnDetail);
             this.Controls.Add(this.chkDatabaz);
