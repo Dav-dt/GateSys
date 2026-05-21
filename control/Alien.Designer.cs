ffa5311bc8e7800a87ead7ac9fcabd3e517064cf
nexxo <fabiolongofabio@gmail.com>
2026-05-21 16:33:19 +0200

    fix taille alien après ajout du style

--- control/Alien.Designer.cs
+++ control/Alien.Designer.cs
@@ -93,8 +93,7 @@
             // 
             // Alien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.Controls.Add(this.lbOutil);
             this.Controls.Add(this.lbDegre);
